c0350d515ee6a7f0b8b237114cddf6fb8320da72
Guillem Andreu <guillem.andreu@ada.com>
2023-05-16 23:23:26 +0200

    feat: add basic security headers

--- next.config.js
+++ next.config.js
@@ -1,12 +1,46 @@
+// @ts-check
+
+const securityHeaders = [
+  {
+    source: '/:path*',
+    headers: [
+      {
+        key: 'Permissions-Policy',
+        value: 'camera=(), geolocation=(), microphone=()',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+      },
+    ],
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     dirs: ['.'],
   },
   experimental: {
-    typedRoutes: true,
+    // typedRoutes: true,
+  },
+  async headers() {
+    return [...securityHeaders]
   },
   productionBrowserSourceMaps: true,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
