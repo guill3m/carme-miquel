0ae654a0f4faadb35d23b7f001ea531d2d1742ba
Guillem Andreu <git@guillemandreu.com>
2024-04-24 22:31:49 +0200

    feat: base MDX setup

---
+++ utils/get-mdx-content.ts
@@ -0,0 +1,20 @@
+import { compileMDX } from 'next-mdx-remote/rsc'
+
+import readMdxFile from '@/utils/read-mdx-file'
+
+export default async function getMdxContent(slug: string, folder: string) {
+  const fileContent = await readMdxFile(slug, folder)
+
+  if (!fileContent) {
+    return null
+  }
+
+  const { content, frontmatter } = await compileMDX({
+    source: fileContent,
+    options: {
+      parseFrontmatter: true,
+    },
+  })
+
+  return { content, frontmatter }
+}
