9733da012268f9f2e33439ff94e3b1daafd0cc2f
Guillem Andreu <git@guillemandreu.com>
2024-04-25 23:37:10 +0200

    feat: page /autora/

---
+++ components/grid.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+import clsx from 'clsx'
+
+import styles from './grid.module.css'
+
+export default function Grid({
+  aside,
+  className,
+  children,
+  title,
+}: Readonly<{
+  aside?: ReactNode
+  className?: string
+  children: ReactNode
+  title: string
+}>) {
+  return (
+    <div className={clsx([className, styles.wrapper])}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.content}>{children}</div>
+      <div className={styles.aside}>{aside}</div>
+    </div>
+  )
+}
