0cf5efe21502098b1ae4842ab115d233c8268879
Guillem Andreu <git@guillemandreu.com>
2024-11-16 20:55:21 +0100

    build: tweak prettier config for yml

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -7,12 +7,6 @@ export default {
 	plugins: ['prettier-plugin-astro'],
 	overrides: [
 		{
-			files: ['.*', '*.md', '*.toml', '*.yml'],
-			options: {
-				useTabs: false,
-			},
-		},
-		{
 			files: '*.astro',
 			options: {
 				parser: 'astro',
