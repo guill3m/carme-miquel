1fce073fc0d768dccd08e2328073500aa1d2990d
Guillem Andreu <git@guillemandreu.com>
2025-02-25 11:16:11 +0100

    feat: add analytics script

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+	readonly UMAMI_ANALYTICS_HOST: string
+	readonly UMAMI_ANALYTICS_ID: string
+	readonly UMAMI_ANALYTICS_SCRIPT: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
