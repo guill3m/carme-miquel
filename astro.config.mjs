2b83cf5c03b0b4658e08046c50bab75a82acfd72
Guillem Andreu <git@guillemandreu.com>
2025-02-27 17:38:24 +0100

    build: enable Astro content intellisense

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
+	experimental: { contentIntellisense: true },
 	integrations: [mdx(), sitemap({ lastmod: new Date() })],
 	site: 'https://www.carmemiquel.com',
 	trailingSlash: 'always',
