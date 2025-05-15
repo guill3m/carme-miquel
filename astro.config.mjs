f59bf52a1dafd4671e198dd2fe19284d9e4cac0a
Guillem Andreu <git@guillemandreu.com>
2025-05-15 10:37:23 +0200

    feat: analytics script is optional based on env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,29 @@
 // @ts-check
 
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
+	env: {
+		schema: {
+			UMAMI_ANALYTICS_HOST: envField.string({
+				context: 'client',
+				access: 'public',
+				optional: true,
+			}),
+			UMAMI_ANALYTICS_ID: envField.string({
+				context: 'client',
+				access: 'public',
+				optional: true,
+			}),
+			UMAMI_ANALYTICS_SCRIPT: envField.string({
+				context: 'client',
+				access: 'public',
+				optional: true,
+			}),
+		},
+	},
 	experimental: { contentIntellisense: true },
 	integrations: [mdx(), sitemap({ lastmod: new Date() })],
 	site: 'https://www.carmemiquel.com',
